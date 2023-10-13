0643f243848117c8aff5232e62b8c715a01096b6
Yan Afriyoko <mail.yanafriyoko@gmail.com>
2023-10-13 23:22:04 +0700

    add confirm modal delete address

--- components/ListAddress.vue
+++ components/ListAddress.vue
@@ -3,7 +3,7 @@
         <div class="row" v-if="listAddress.length > 0">
             <div :class="col" v-for="item in listAddress" :key="item.id">
                 <div class="border rounded-lg position-relative p-3 mb-3" :class="{ 'border-primary bg-transparent-primary': item.id == (type == 'shipping' ? selectedAddressId : selectedBillingId) }">
-                    <button class="btn position-absolute p-0" @click="removeAddress(item.id)" style="right: 1rem; top: 1rem">
+                    <button class="btn position-absolute p-0" @click="handleDelete(item.id)" style="right: 1rem; top: 1rem">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                             <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
@@ -29,6 +29,17 @@
                 <FormAddress method="PUT" :detailAddress="detailAddress" />
             </div>
         </b-modal>
+        <b-modal id="modalConfirm" title="Confirmation" size="md" centered>
+            <template #default>
+                <p class="mb-0">Are you sure to delete this address ?</p>
+            </template>
+            <template #modal-footer>
+                <div class="d-flex align-items-center">
+                    <button class="btn btn-md btn-info mr-3" @click="closeModal()">Cancel</button>
+                    <button class="btn btn-md btn-danger" @click="removeAddress()" >Delete</button>
+                </div>
+            </template>
+        </b-modal>
     </div>
 </template>
 
@@ -43,6 +54,7 @@ export default {
     data() {
         return {
             detailAddress: {},
+            getId: "",
         };
     },
     methods: {
@@ -66,8 +78,15 @@ export default {
                 this.$bvModal.hide("modalListBilling");
             }
         },
-        removeAddress(id) {
-            let payload = { by_id: id };
+        handleDelete(by_id) {
+            this.getId = by_id;
+            this.$bvModal.show("modalConfirm");
+        },
+        closeModal() {
+            this.$bvModal.hide("modalConfirm");
+        },
+        removeAddress() {
+            let payload = { by_id: this.getId };
 
             return this.$axios
                 .post("/api/v1/private/users/shipping-address/destroy", payload)
@@ -77,10 +96,12 @@ export default {
                         variant: "success",
                         solid: true,
                     });
+                    this.$bvModal.hide("modalConfirm");
                     this.$store.dispatch("showProfile");
                 })
                 .catch((err) => {
-                    console.log(err);
+                    // console.log(err);
+                    this.$bvModal.hide("modalConfirm");
                     if (err?.response?.data?.data) {
                         err.response.data.data.forEach((el) => {
                             this.$bvToast.toast(el.message, {
