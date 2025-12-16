83d48afa24d4426c397843066d8f0875d7c6cf02
root <you@example.com>
2025-12-16 15:41:56 +0000

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,38 @@
+//module.exports = {
+//    apps: [
+//      {
+  //      name: 'jagoanvoucher.com',
+//        exec_mode: 'cluster',
+//        instances: '1', // Or a number of instances
+ //       script: './node_modules/nuxt/bin/nuxt.js',
+  //      args: 'start'
+  //    }
+  //  ]
+ // }
+
 module.exports = {
-    apps: [
-      {
-        name: 'staging.ecommerce.com',
-        exec_mode: 'cluster',
-        instances: '1', // Or a number of instances
-        script: './node_modules/nuxt/bin/nuxt.js',
-        args: 'start'
-      }
-    ]
-  }
-  
+	apps: [
+		{
+			name: 'jagoanvoucher.com',
+
+			// PENTING
+			exec_mode: 'fork',
+			instances: 1,
+
+			script: './node_modules/nuxt/bin/nuxt.js',
+			args: 'start',
+
+			// LIMIT RAM
+			max_memory_restart: '220M',
+
+			// LIMIT HEAP NODE
+			node_args: '--max-old-space-size=200',
+
+			// Optional tapi bagus
+			autorestart: true,
+			watch: false
+		}
+	]
+}
+
+
